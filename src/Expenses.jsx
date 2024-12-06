c99c4d54b2343b8378e9ecbe322c0b2f339ccac8
yatishb23 <yatishbad232@gmail.com>
2024-12-07 00:47:59 +0530

    third

--- src/Expenses.jsx
+++ src/Expenses.jsx
@@ -16,7 +16,6 @@ import {
   AppBar,
   Toolbar,
 } from "@mui/material";
-import { Chart as ChartJS } from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 import { Add, Delete } from "@mui/icons-material";
 import { useSearchParams, useNavigate } from "react-router-dom";
